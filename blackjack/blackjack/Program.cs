c11d8aec4e69c711311201042123efa79199d1cd
Jeff Longo <jeffreylongo@yahoo.com>
2017-03-23 16:10:48 -0400

    for loop gone

--- blackjack/blackjack/Program.cs
+++ blackjack/blackjack/Program.cs
@@ -31,10 +31,7 @@ namespace blackjack
             var gameDeck = Deck();
             List<Card> userHand = new List<Card>();
 
-            for (int i = 0; i < length; i++)
-            {
-
-            }
+            
 
 
 
