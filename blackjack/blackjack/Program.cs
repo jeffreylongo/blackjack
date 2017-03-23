4181a7450235d9957d41ac6f1bc296dc731bcbc7
Jeff Longo <jeffreylongo@yahoo.com>
2017-03-23 14:56:53 -0400

    set  up first method for pulling the randomized deck

--- blackjack/blackjack/Program.cs
+++ blackjack/blackjack/Program.cs
@@ -8,9 +8,30 @@ namespace blackjack
 {
     class Program
     {
+        static List<Card> Deck()
+        {
+            var deck = new List<Card>();
+
+            foreach (Rank r in Enum.GetValues(typeof(Rank)))
+            {
+                foreach (Suit s in Enum.GetValues(typeof(Suit)))
+                {
+                    deck.Add(new Card(s, r));
+                }
+            }
+
+            //sort the deck. NOTICE that the variable 'deck' is unchanged, but 'randomDeck' is the actual sorted deck.
+            var randomDeck = deck.OrderBy(x => Guid.NewGuid()).ToList();
+
+            return randomDeck;
+        }
+
+
         static void Main(string[] args)
         {
-
+            
+            
+            Console.ReadLine();
         }
     }
 }
